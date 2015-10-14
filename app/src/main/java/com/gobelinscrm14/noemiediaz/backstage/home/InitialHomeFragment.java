e652f081f136c65bda4df36968e3065b76aa1b3b
NoemieD <noemiediaz4@gmail.com>
2015-10-14 15:11:09 +0200

    initial homepage

---
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/home/InitialHomeFragment.java
@@ -0,0 +1,31 @@
+package com.gobelinscrm14.noemiediaz.backstage.home;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gobelinscrm14.noemiediaz.backstage.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InitialHomeFragment extends Fragment {
+
+
+    public InitialHomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_initial_home, container, false);
+    }
+
+
+}
