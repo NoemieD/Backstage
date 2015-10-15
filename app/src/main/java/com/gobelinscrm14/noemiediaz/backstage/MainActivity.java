2f5f37cb47deb672f4c1f517b7af859d6f6af322
NoemieD <noemiediaz4@gmail.com>
2015-10-15 11:25:05 +0200

    Add Toolbar, Main Menu

--- app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/MainActivity.java
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.firebase.client.AuthData;
@@ -24,11 +27,53 @@ public class MainActivity extends AppCompatActivity implements InitialHomeFragme
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //ADD TOOLBAR
+        Toolbar mtoolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
         //ADD INITIAL FRAGMENT ON MAIN ACTIVITY TO CREATE THE HOMEPAGE
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.mainContent, new InitialHomeFragment())
                 .commit();
+    }
+
+    //CREATE MENU
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //OPTION MENU
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.menuMainHomeItem) {
+            //Home Clicked
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.mainContent, new InitialHomeFragment())
+                    .commit();
+
+            return true;
+        } else if (item.getItemId() == R.id.menuMainLoginItem) {
+            // Login Clicked
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.mainContent, new LoginFragment())
+                    .commit();
+            return true;
+        }
+        else if (item.getItemId() == R.id.menuMainRegisterItem) {
+            //Register Clicked
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.mainContent, new RegisterFragment())
+                    .commit();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
