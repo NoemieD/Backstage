3334bdd5bf4c012deda99018ca1461e1ed5d5fa3
NoemieD <noemiediaz4@gmail.com>
2015-10-15 09:27:49 +0200

    Add listener on click

--- app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/MainActivity.java
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/MainActivity.java
@@ -1,11 +1,14 @@
 package com.gobelinscrm14.noemiediaz.backstage;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.gobelinscrm14.noemiediaz.backstage.home.InitialHomeFragment;
+import com.gobelinscrm14.noemiediaz.backstage.home.user.LoginFragment;
+import com.gobelinscrm14.noemiediaz.backstage.home.user.RegisterFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InitialHomeFragment.InitialHomeListener{
 
     private static final String TAG = "mainActivity";
 
@@ -18,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.mainContent, new InitialHomeFragment())
+                .commit();
+
+    }
+
+    @Override
+    public void onRegisterClicked() {
+        //ADD REGISTER FRAGMENT AFTER CLICK
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .replace(R.id.mainContent, new RegisterFragment())
+                .commit();
+    }
+
+    @Override
+    public void onLoginClicked() {
+        //ADD LOG FRAGMENT AFTER CLICK
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .replace(R.id.mainContent, new LoginFragment())
                 .commit();
     }
 }
--- app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/home/InitialHomeFragment.java
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/home/InitialHomeFragment.java
@@ -1,19 +1,31 @@
 package com.gobelinscrm14.noemiediaz.backstage.home;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gobelinscrm14.noemiediaz.backstage.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InitialHomeFragment extends Fragment {
 
+
+    private static final String TAG = "initialHomeFragment";
+
+    private InitialHomeListener mListener;
+
+    @Bind(R.id.buttonRegister) Button buttonRegister;
 
     public InitialHomeFragment() {
         // Required empty public constructor
@@ -24,8 +36,41 @@ public class InitialHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_initial_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_initial_home, container, false);
+
+        ButterKnife.bind(this, view);
+
+        return view;
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        //context -> activity
+        try{
+            mListener = (InitialHomeListener) context;
+        } catch (ClassCastException exception){
+            throw new ClassCastException(context.toString()
+                    + "must implement LoginFragment.LoginListener");
+        }
+    }
+
+    public interface InitialHomeListener{
+        void onRegisterClicked();
+        void onLoginClicked();
+    }
+
+
+    @OnClick(R.id.buttonRegister)
+    public void onRegisterClick(View v){
+        mListener.onRegisterClicked();
+    }
+
+    @OnClick(R.id.buttonLogin)
+    public void onLoginClick(View v){
+        mListener.onLoginClicked();
+    }
+
 
 
 }
