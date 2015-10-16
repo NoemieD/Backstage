96102364a3836e1468e495733b7ddeb2e999fbbe
NoemieD <noemiediaz4@gmail.com>
2015-10-16 14:20:28 +0200

    put chat on fragment

--- app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/chat/ChatActivity.java
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/chat/ChatActivity.java
@@ -1,139 +1,26 @@
 package com.gobelinscrm14.noemiediaz.backstage.chat;
 
-import android.app.ListActivity;
-import android.content.SharedPreferences;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
-import com.gobelinscrm14.noemiediaz.backstage.Authentification;
 import com.gobelinscrm14.noemiediaz.backstage.R;
-import com.gobelinscrm14.noemiediaz.backstage.User;
 
-import java.util.Random;
-
-public class ChatActivity extends ListActivity {
+public class ChatActivity extends AppCompatActivity {
 
     private static final String TAG = "chatActivity";
-    private static final String FIREBASE_URL = "https://backstagecrm14.firebaseio.com/";
-
-    private String mPseudo;
-    private Firebase mFirebaseRef;
-    private ValueEventListener mConnectedListener;
-    private ChatListAdapter mChatListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        // Make sure we have a mUsername
-        setupPseudo();
-
-        // Setup our Firebase mFirebaseRef
-        mFirebaseRef = new Firebase(FIREBASE_URL).child("chat");
-
-        // Setup our input methods. Enter key on the keyboard or pushing the send button
-        EditText inputText = (EditText) findViewById(R.id.messageInput);
-        inputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-                if (actionId == EditorInfo.IME_NULL && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
-                    sendMessage();
-                }
-                return true;
-            }
-        });
-
-        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendMessage();
-            }
-        });
-
-        //ADD TOOLBAR
-        /*Toolbar mtoolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mtoolbar);*/
-
-       /* getSupportFragmentManager()
+       getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.mainContent, new UserProfilFragment())
-                .commit();*/
+                .add(R.id.mainContent, new ChatListFragment())
+                .commit();
 
 
     }
 
-    private void setupPseudo() {
-        User user = Authentification.getInstance().getUser();
-        mPseudo = user.getPseudo();
-
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
-        final ListView listView = getListView();
-        // Tell our list adapter that we only want 50 messages at a time
-        mChatListAdapter = new ChatListAdapter(mFirebaseRef.limit(50), this, R.layout.chat_message, mPseudo);
-        listView.setAdapter(mChatListAdapter);
-        mChatListAdapter.registerDataSetObserver(new DataSetObserver() {
-            @Override
-            public void onChanged() {
-                super.onChanged();
-                listView.setSelection(mChatListAdapter.getCount() - 1);
-            }
-        });
-
-        // Finally, a little indication of connection status
-        mConnectedListener = mFirebaseRef.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                boolean connected = (Boolean) dataSnapshot.getValue();
-                if (connected) {
-                    Toast.makeText(ChatActivity.this, "Connected to Firebase", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(ChatActivity.this, "Disconnected from Firebase", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-                // No-op
-            }
-        });
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mFirebaseRef.getRoot().child(".info/connected").removeEventListener(mConnectedListener);
-        mChatListAdapter.cleanup();
-    }
-
-    private void sendMessage() {
-        EditText inputText = (EditText) findViewById(R.id.messageInput);
-        String input = inputText.getText().toString();
-        if (!input.equals("")) {
-            // Create our 'model', a Chat object
-            Chat chat = new Chat(input, mPseudo);
-            // Create a new, auto-generated child of that chat location, and save our chat data there
-            mFirebaseRef.push().setValue(chat);
-            inputText.setText("");
-        }
-    }
 
 
 }
--- app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/chat/ChatListFragment.java
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/chat/ChatListFragment.java
@@ -1,19 +1,45 @@
 package com.gobelinscrm14.noemiediaz.backstage.chat;
 
-
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.gobelinscrm14.noemiediaz.backstage.Authentification;
 import com.gobelinscrm14.noemiediaz.backstage.R;
+import com.gobelinscrm14.noemiediaz.backstage.User;
+
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ChatListFragment extends Fragment {
+public class ChatListFragment extends ListFragment {
 
+    private static final String TAG = "chatListFragment";
+    private static final String FIREBASE_URL = "https://backstagecrm14.firebaseio.com/";
+
+    //private ChartListListener mListener;
+
+    private String mPseudo;
+    private Firebase mFirebaseRef;
+    private ValueEventListener mConnectedListener;
+    private ChatListAdapter mChatListAdapter;
+    private EditText inputText;
 
     public ChatListFragment() {
         // Required empty public constructor
@@ -24,7 +50,112 @@ public class ChatListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
+
+        ButterKnife.bind(this, view);
+
+        // Make sure we have a mUsername
+        setupPseudo();
+
+        // Setup our Firebase mFirebaseRef
+        mFirebaseRef = new Firebase(FIREBASE_URL).child("chat");
+
+        // Setup our input methods. Enter key on the keyboard or pushing the send button
+        inputText = (EditText) view.findViewById(R.id.messageInput);
+        inputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_NULL && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    sendMessage();
+                }
+                return true;
+            }
+        });
+
+        view.findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+
+
+        return view;
+    }
+
+   /* @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        //context -> activity
+        try{
+            mListener = (ChartListListener) context;
+        } catch (ClassCastException exception){
+            throw new ClassCastException(context.toString()
+                    + "must implement LoginFragment.LoginListener");
+        }
+    }
+
+    public interface ChartListListener{
+
+    }*/
+
+    private void setupPseudo() {
+        User user = Authentification.getInstance().getUser();
+        mPseudo = user.getPseudo();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
+        final ListView listView = getListView();
+        // Tell our list adapter that we only want 50 messages at a time
+        mChatListAdapter = new ChatListAdapter(mFirebaseRef.limit(50), getActivity(), R.layout.chat_message, mPseudo);
+        listView.setAdapter(mChatListAdapter);
+        mChatListAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(mChatListAdapter.getCount() - 1);
+            }
+        });
+
+        // Finally, a little indication of connection status
+        mConnectedListener = mFirebaseRef.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean connected = (Boolean) dataSnapshot.getValue();
+                if (connected) {
+                    Toast.makeText(getActivity(), "Connected to Firebase", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "Disconnected from Firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                // No-op
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mFirebaseRef.getRoot().child(".info/connected").removeEventListener(mConnectedListener);
+        mChatListAdapter.cleanup();
+    }
+
+    private void sendMessage() {
+        String input = inputText.getText().toString();
+        if (!input.equals("")) {
+            // Create our 'model', a Chat object
+            Chat chat = new Chat(input, mPseudo);
+            // Create a new, auto-generated child of that chat location, and save our chat data there
+            mFirebaseRef.push().setValue(chat);
+            inputText.setText("");
+        }
     }
 
 
