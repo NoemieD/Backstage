8d3f01af00150f13d071436deb223434dade7bbd
NoemieD <noemiediaz4@gmail.com>
2015-10-16 10:02:57 +0200

    Add Abstract Class Firebase List Adapter

---
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/backstage/chat/FirebaseListAdapter.java
@@ -0,0 +1,122 @@
+package com.gobelinscrm14.noemiediaz.backstage.chat;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by noemiediaz on 16/10/15.
+ */
+public abstract class FirebaseListAdapter<T> extends BaseAdapter {
+
+    private Query mRef;
+    private Class<T> mModelClass;
+    private int mLayout;
+    private LayoutInflater mInflater;
+    private List<T> mModels;
+    private List<String> mKeys;
+    private ChildEventListener mListener;
+
+
+    public FirebaseListAdapter(Query mRef, Class<T> mModelClass, int mLayout, Activity activity) {
+        this.mRef = mRef;
+        this.mModelClass = mModelClass;
+        this.mLayout = mLayout;
+        mInflater = activity.getLayoutInflater();
+        mModels = new ArrayList<T>();
+        mKeys = new ArrayList<String>();
+        // Look for all child events. We will then map them to our own internal ArrayList, which backs ListView
+        mListener = this.mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+
+                T model = dataSnapshot.getValue(FirebaseListAdapter.this.mModelClass);
+                String key = dataSnapshot.getKey();
+
+                // Insert into the correct location, based on previousChildName
+                if (previousChildName == null) {
+                    mModels.add(0, model);
+                    mKeys.add(0, key);
+                } else {
+                    int previousIndex = mKeys.indexOf(previousChildName);
+                    int nextIndex = previousIndex + 1;
+                    if (nextIndex == mModels.size()) {
+                        mModels.add(model);
+                        mKeys.add(key);
+                    } else {
+                        mModels.add(nextIndex, model);
+                        mKeys.add(nextIndex, key);
+                    }
+                }
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                // One of the mModels changed. Replace it in our list and name mapping
+                String key = dataSnapshot.getKey();
+                T newModel = dataSnapshot.getValue(FirebaseListAdapter.this.mModelClass);
+                int index = mKeys.indexOf(key);
+
+                mModels.set(index, newModel);
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                // A model was removed from the list. Remove it from our list and the name mapping
+                String key = dataSnapshot.getKey();
+                int index = mKeys.indexOf(key);
+
+                mKeys.remove(index);
+                mModels.remove(index);
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+
+                // A model changed position in the list. Update our list accordingly
+                String key = dataSnapshot.getKey();
+                T newModel = dataSnapshot.getValue(FirebaseListAdapter.this.mModelClass);
+                int index = mKeys.indexOf(key);
+                mModels.remove(index);
+                mKeys.remove(index);
+                if (previousChildName == null) {
+                    mModels.add(0, newModel);
+                    mKeys.add(0, key);
+                } else {
+                    int previousIndex = mKeys.indexOf(previousChildName);
+                    int nextIndex = previousIndex + 1;
+                    if (nextIndex == mModels.size()) {
+                        mModels.add(newModel);
+                        mKeys.add(key);
+                    } else {
+                        mModels.add(nextIndex, newModel);
+                        mKeys.add(nextIndex, key);
+                    }
+                }
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e("FirebaseListAdapter", "Listen was cancelled, no more updates will occur");
+            }
+
+        });
+    }
+}
